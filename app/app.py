cf7ad8871cde9b0f831775a35c288578fb0e6735
Robbie <txt3rob@gmail.com>
2018-02-08 14:37:08 +0000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -6,8 +6,10 @@ import re
 import requests
 
 
-mysql = MySQL()
+
 app = Flask(__name__)
+ask = Ask(app, "/")
+logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 
 @app.route('/')
 def main():
@@ -28,134 +30,32 @@ def showSignin():
 def showAddWish():
     return render_template('addWish.html')
 
+################### ALEXA SKILLS SECTION #####################
 
-@app.route('/userHome')
-def userHome():
-    if session.get('user'):
-        return render_template('userHome.html')
-    else:
-        return render_template('error.html',error = 'Unauthorized Access')
+@ask.intent('AMAZON.HelpIntent')
+def help():
+    help_text = render_template('help')
+    return question(help_text).reprompt(help_text)
 
 
-@app.route('/logout')
-def logout():
-    session.pop('user',None)
-    return redirect('/')
+@ask.intent('AMAZON.StopIntent')
+def stop():
+    bye_text = render_template('bye')
+    return statement(bye_text)
 
-@app.route('/validateLogin',methods=['POST'])
-def validateLogin():
-    try:
-        _username = request.form['inputEmail']
-        _password = request.form['inputPassword']
 
-        # connect to mysql
+@ask.intent('AMAZON.CancelIntent')
+def cancel():
+    bye_text = render_template('bye')
+    return statement(bye_text)
 
-        con = mysql.connect()
-        cursor = con.cursor()
-        cursor.callproc('sp_validateLogin',(_username,))
-        data = cursor.fetchall()
 
-        if len(data) > 0:
-            if check_password_hash(str(data[0][3]),_password):
-                session['user'] = data[0][0]
-                return redirect('/userHome')
-            else:
-                return render_template('error.html',error = 'Wrong Email address or Password.')
-        else:
-            return render_template('error.html',error = 'Wrong Email address or Password.')
-
-    except Exception as e:
-        return render_template('error.html',error = str(e))
-    finally:
-        cursor.close()
-        con.close()
+@ask.session_ended
+def session_ended():
+    return "{}", 200
 
 
 
-@app.route('/signUp',methods=['POST','GET'])
-def signUp():
-    try:
-        _name = request.form['inputName']
-        _email = request.form['inputEmail']
-        _password = request.form['inputPassword']
-
-        # validate the received values
-        if _name and _email and _password:
-
-            # All Good, let's call MySQL
-
-            conn = mysql.connect()
-            cursor = conn.cursor()
-            _hashed_password = generate_password_hash(_password)
-            cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
-            data = cursor.fetchall()
-
-            if len(data) is 0:
-                conn.commit()
-                return json.dumps({'message':'User created successfully !'})
-            else:
-                return json.dumps({'error':str(data[0])})
-        else:
-            return json.dumps({'html':'<span>Enter the required fields</span>'})
-
-    except Exception as e:
-        return json.dumps({'error':str(e)})
-    finally:
-        cursor.close()
-        conn.close()
-
-@app.route('/getWish')
-def getWish():
-    try:
-        if session.get('user'):
-            _user = session.get('user')
-
-            con = mysql.connect()
-            cursor = con.cursor()
-            cursor.callproc('sp_GetWishByUser',(_user,))
-            wishes = cursor.fetchall()
-
-            wishes_dict = []
-            for wish in wishes:
-                wish_dict = {
-                        'Id': wish[0],
-                        'Title': wish[1],
-                        'Description': wish[2],
-                        'Date': wish[4]}
-                wishes_dict.append(wish_dict)
-
-            return json.dumps(wishes_dict)
-        else:
-            return render_template('error.html', error = 'Unauthorized Access')
-    except Exception as e:
-        return render_template('error.html', error = str(e))
-
-@app.route('/addWish',methods=['POST'])
-def addWish():
-    try:
-        if session.get('user'):
-            _title = request.form['inputTitle']
-            _description = request.form['inputDescription']
-            _user = session.get('user')
-
-            conn = mysql.connect()
-            cursor = conn.cursor()
-            cursor.callproc('sp_addWish',(_title,_description,_user))
-            data = cursor.fetchall()
-
-            if len(data) is 0:
-                conn.commit()
-                return redirect('/userHome')
-            else:
-                return render_template('error.html',error = 'An error occurred!')
-
-        else:
-            return render_template('error.html',error = 'Unauthorized Access')
-    except Exception as e:
-        return render_template('error.html',error = str(e))
-    finally:
-        cursor.close()
-        conn.close()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5002)
